592bc104194c8de88bdd35139f8b0ac5aa4521f2
athouvenin <agnes.thouvenin@insa-lyon.fr>
2021-11-12 10:36:17 +0100

    ajout seance 3

---
+++ TP3: Mécanismes TCP/seance3/client3.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#define RCVSIZE 1024
+
+int main (int argc, char *argv[]) {
+
+    struct sockaddr_in adresse;
+    int port = 5001;
+    int valid = 1;
+    char s_buffer[RCVSIZE];
+    char c_buffer[RCVSIZE];
+    char syn[RCVSIZE] = "SYN";
+    char ack[RCVSIZE] = "ACK";
+
+    int server_struct_length = sizeof(adresse);
+
+    memset(s_buffer, 0, sizeof(s_buffer));
+    //memset(c_buffer, 0, sizeof(c_buffer));
+
+    // (1) create socket
+    int client_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    // (2) handle error
+    if (client_desc < 0) {
+        perror("cannot create socket\n");
+        return -1;
+    }
+
+    // (3) fonction pour réutiliser immédiatement la socket
+    setsockopt(client_desc, SOL_SOCKET, SO_REUSEADDR, &valid, sizeof(int));
+
+    // (4) faire le lien entre la socket et le couple (@IP, port) avec la structure sockaddr (coté serveur)s
+    adresse.sin_family= AF_INET;
+    adresse.sin_port= htons(port);
+    adresse.sin_addr.s_addr= htonl(INADDR_ANY);
+
+
+    // (6) recevoir et transmettre des messages
+
+    // [1] Envoie du "SYN" au serveur
+    sendto(client_desc, syn, strlen(syn), 0,
+            (struct sockaddr*)&adresse, server_struct_length);
+            
+    //int cont = 1;
+    while(1){ 
+
+        // [2] Afficher le "SYN-ACK" du serveur
+
+        if(recvfrom(client_desc, s_buffer, sizeof(s_buffer), 0,
+            (struct sockaddr*)&adresse, &server_struct_length) < 0){
+            printf("Error while receiving server's msg\n");
+            return -1;
+        }
+
+        if (strcmp(s_buffer,"SYN-ACK") == 0){
+            printf("Response from server: %s\n",s_buffer);
+
+            // [3] Renvoie du "ACK" au serveur
+            sendto(client_desc, ack, strlen(ack), 0,
+            (struct sockaddr*)&adresse, server_struct_length);
+
+            //***********************************************
+            fgets(c_buffer, RCVSIZE, stdin);   
+            if(sendto(client_desc, c_buffer, strlen(c_buffer), 0,
+                (struct sockaddr*)&adresse, server_struct_length) < 0){
+                printf("Unable to send message\n");
+                return -1;
+            }
+            
+            memset(s_buffer,0,RCVSIZE);
+
+            // Receive the server's response:
+            if(recvfrom(client_desc, s_buffer, sizeof(s_buffer), 0,
+                (struct sockaddr*)&adresse, &server_struct_length) < 0){
+                printf("Error while receiving server's msg\n");
+                return -1;
+            }
+            
+            printf("Server's response: %s\n", s_buffer);
+            }
+    }
+    // Close the socket:
+    close(client_desc);
+    
+    return 0;
+}
---
+++ TP3: Mécanismes TCP/seance3/serveur3.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#define RCVSIZE 1024
+
+int main(int argc, char *argv[]){
+    
+    /*int socket_desc;
+    struct sockaddr_in server_addr, client_addr;
+    char server_message[2000], client_message[2000];
+    int client_struct_length = sizeof(client_addr);*/
+    
+    // déclaration des variables
+    struct sockaddr_in server_addr, client_addr;
+    int port= 5001;
+    int valid= 1;
+    socklen_t alen= sizeof(client_addr);
+    char s_buffer[RCVSIZE];
+    char c_buffer[RCVSIZE];
+    char synack[RCVSIZE] = "SYN-ACK";
+
+    memset(s_buffer, 0, sizeof(s_buffer));
+    memset(c_buffer, 0, sizeof(c_buffer));
+
+    // (1) create socket
+    int server_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    // afficher valeur descripteur
+    // printf ("valeur descripteur: %d\n", server_desc);
+
+    // (2) handle error
+    if (server_desc < 0) {
+        perror("Cannot create socket\n");
+        return -1;
+    }
+
+    // (3) fonction pour réutiliser immédiatement la socket
+    setsockopt(server_desc, SOL_SOCKET, SO_REUSEADDR, &valid, sizeof(int));
+
+    // (4) faire le lien entre la socket et le couple (@IP, port) avec la structure sockaddr (coté serveur)
+    server_addr.sin_family= AF_INET;
+    server_addr.sin_port= htons(port);
+    server_addr.sin_addr.s_addr= htonl(INADDR_ANY);
+
+    // (5) initialize socket
+    if (bind(server_desc, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
+        perror("Bind failed\n");
+        close(server_desc);
+        return -1;
+    }   
+    printf("Listen done\n");
+
+    // (6) Receive client's message:
+    //printf("Accepting\n");
+
+    if (recvfrom(server_desc, c_buffer, sizeof(c_buffer), 0,
+        (struct sockaddr*)&client_addr, &alen) < 0){
+        printf("Couldn't receive\n");
+        return -1;
+    }
+
+
+    // [1] Recevoir le SYN du client
+    printf("Received message from IP: %s and port: %i\n",
+        inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        
+    if (strcmp(c_buffer,"SYN") == 0){
+        printf("SYN from client: %s\n",c_buffer);
+        
+        memset(c_buffer,0,RCVSIZE);
+
+        // [2] Respond "SYN-ACK" to client
+        sendto(server_desc, synack, strlen(synack), 0,
+            (struct sockaddr*)&client_addr, alen);
+        
+
+        while(1){ 
+
+            if (recvfrom(server_desc, c_buffer, sizeof(c_buffer), 0,
+                (struct sockaddr*)&client_addr, &alen) < 0){
+                printf("Couldn't receive\n");
+                return -1;
+            } 
+            //printf("contenu c_buffer: %s\n",c_buffer);  
+
+            if (strcmp(c_buffer,"ACK") == 0){
+                printf("ACK from client: %s\n",c_buffer);
+
+                memset(c_buffer,0,RCVSIZE);
+
+                //***************************
+                printf("Accepting\n");
+
+                if (recvfrom(server_desc, c_buffer, sizeof(c_buffer), 0,
+                    (struct sockaddr*)&client_addr, &alen) < 0){
+                    printf("Couldn't receive\n");
+                    return -1;
+                }
+                printf("Received message from IP: %s and port: %i\n",
+                    inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                
+                printf("Msg from client: %s\n",c_buffer);
+                
+                memset(c_buffer,0,RCVSIZE);
+
+                // Respond to client:
+                fgets(s_buffer, RCVSIZE, stdin);
+                //strcpy(s_buffer, c_buffer);
+                
+                if (sendto(server_desc, s_buffer, strlen(s_buffer), 0,
+                    (struct sockaddr*)&client_addr, alen) < 0){
+                    printf("Can't send\n");
+                    return -1;
+                }
+            }
+        }
+            
+    }
+    
+    
+    // Close the socket:
+    close(server_desc);
+    return 0;
+}
